88f3c9f531e6a19af4528e1c89935c730e53b9b9
ntt-congnt <cong.nv@nttnetworks.com>
2025-06-23 23:01:10 +0700

    Update API endpoints and CORS configuration

--- api/index.ts
+++ api/index.ts
@@ -211,24 +211,45 @@ app.get("/api2/search", async (req, res) => {
         return res.status(400).send({ status: "fail", message: "Missing query parameter 'q'" });
     }
 
-    const url = `https://fo4s.com/ajax?action=search_player&input=${encodeURIComponent(JSON.stringify({ q: playerName }))}`;
+    // const url = `https://fo4s.com/ajax?action=search_player&input=${encodeURIComponent(JSON.stringify({ q: playerName }))}`;
 
-    try {
-        const response = await axios.get(url, {
-            headers: {
-                'accept': 'application/json, text/plain, */*',
-                'user-agent': req.headers['user-agent'] || 'Mozilla/5.0'
+    let config = {
+        method: 'get',
+        maxBodyLength: Infinity,
+        url: `https://fo4s.com/ajax?action=search_player&input=%7B%22pos%22%3A%5B%5D%2C%22class%22%3A%5B%5D%2C%22league%22%3A%22%22%2C%22club%22%3A%22%22%2C%22nation%22%3A%22%22%2C%22team%22%3A%22%22%2C%22trait%22%3A%5B%22%22%2C%22%22%2C%22%22%5D%2C%22ig_trait%22%3A%5B%5D%2C%22attr%22%3A%5B%22%22%2C%22%22%2C%22%22%5D%2C%22attr_value%22%3A%5B%7B%7D%2C%7B%7D%2C%7B%7D%5D%2C%22lfoot%22%3A%22%22%2C%22rfoot%22%3A%22%22%2C%22month%22%3A%22%22%2C%22day%22%3A%22%22%2C%22build%22%3A%5B%5D%2C%22skill%22%3A%22%22%2C%22fame%22%3A%22%22%2C%22sort%22%3A%22ovr-desc%22%2C%22col1%22%3A%22sprintspeed%22%2C%22col2%22%3A%22stamina%22%2C%22col3%22%3A%22strength%22%2C%22q%22%3A%22${encodeURIComponent(playerName)}%22%7D`,
+        headers: { 
+                'accept': 'application/json, text/plain, */*', 
+                'accept-language': 'en-US,en;q=0.9,vi;q=0.8', 
+                'dnt': '1', 
+                'priority': 'u=1, i', 
+                'referer': 'https://fo4s.com/?sort=ovr-desc&col1=sprintspeed&col2=stamina&col3=strength&q=Diego+Maradona', 
+                'sec-ch-ua': '"Chromium";v="136", "Microsoft Edge";v="136", "Not.A/Brand";v="99"', 
+                'sec-ch-ua-mobile': '?0', 
+                'sec-ch-ua-platform': '"Windows"', 
+                'sec-fetch-dest': 'empty', 
+                'sec-fetch-mode': 'cors', 
+                'sec-fetch-site': 'same-origin', 
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36 Edg/136.0.0.0', 
+                'x-requested-with': 'XMLHttpRequest', 
+                'x-xsrf-token': 'eyJpdiI6IlpXMGZpelVuUDYyZm02c1wvaGpOQVVRPT0iLCJ2YWx1ZSI6Im00RW44RmJiK2QwamZtV3VLNHlcL2xVUU9YWXd2S0FoUFI4Uk5oYkxrU200U2hCczFJam04TkFcLzVzRndHNXEzNSIsIm1hYyI6IjVkZTczNTk0ZmJkYjQ2MzZkNzZmMjc5NGM1MWI3YzFhZDdkM2NmODJjMGJmYmNkNjZmZWQ5YTk0ZjY4MTgxZGQifQ==', 
+                'Cookie': '_gid=GA1.2.171347783.1750691001; cf_clearance=zzaJ_v71h8Xh3YXxASlRIFHP4O5UN798SugjyNM.aos-1750693589-1.2.1.1-rjnAXFpMDPTQ3xoq9wrmCsqkUueH0kp3PA6L98zq5C4XySBFTiUkkaI9a40t.HqEGP2dmamTGGRc2Bb3TLZ5AlRyiOvVx_6IbQKiaJyr7sL19.Z74DB_TlLvXXKG1W5mjKHMz1FJiMxSKlU6BfZhi6v0f_OoYYXk9eveNsJmY8aZfJkK1sRCL_SOOkMAh8Ae3lHcFw3Z3OF6ZC9QTOrKokwHv494mLGQ3METz.aZoQjxXGT.R0BKItYBaYRWXgA42_RJxGIQRDxODvzbscbFeLn8ZoiLUa9iahn6.WkKCyDjtopZwzifYPqXKmkJ5BP5Y1fxzMihV87YjR59u1uwo2uxc9a.IBhxQuixa5JHh60; _gat_gtag_UA_188129913_2=1; _ga=GA1.1.1940381296.1750475509; _ga_58C26BMH3G=GS2.1.s1750693589$o4$g1$t1750694010$j47$l0$h0; XSRF-TOKEN=eyJpdiI6IjJTbEZ1S2E4R0pyZVlaT3FqTGlQblE9PSIsInZhbHVlIjoiZXE4bzVOWCtoWTVwWEJMRHRRSjZVdUQ5Z0RobzQrZ2FOS1V1TWlENFMrUURQNHhxOWN4Y0U4VndJemxYNVltMyIsIm1hYyI6Ijc2ODBkYWFmMjE0YjJmNzJmMzliZmU3ZGEyMGRiYzJkMDg4YTA4MzE1YzNlZjI3MjIxMmJiN2UxNzRkYjRkMjAifQ%3D%3D; fo4s_session=eyJpdiI6Im9KOTRaS3FCeVF0em02eTR6WXhFcVE9PSIsInZhbHVlIjoibGN4WU9UeWVCVFJ0a0d6MlErTHpUNUorNWNXSDBHNEcxYU1xdjNkMmNwUHI2ZkxSUTJWSHpzQ1IzNk9IbmVrRiIsIm1hYyI6IjM4NmFlNzI4OGZlZjM3ZGNhOTk3OTE2M2MyMjU1ZmU0YjUyYzg0YzFlNmQ0YjY4NGZlNWFmODNkNzRhNzAzMmMifQ%3D%3D; XSRF-TOKEN=eyJpdiI6ImVLd3VBa2R0aVpiZjBWS1VHakpIMGc9PSIsInZhbHVlIjoiZ0NwVlh0a2wwRmpya3hlNDdtQ1wvdERQSGFVQzNCTUg0aFhDV01WUHlXUDFFUEFvRnVLT2RDQk9taDFLUUEzUW4iLCJtYWMiOiJkMTJlYWQ4NTI5MzQ4NDUxMTM4MTQxZTY3ZTNkZmE5Mjg1NTNlNDM3Yzc4MWJhNWYyNWJhNGRhMTQ5YjcxYjNlIn0%3D; fo4s_session=eyJpdiI6ImsxaEwrSUJ5dzUrSnQxU2tNS1wvUlBBPT0iLCJ2YWx1ZSI6InVvSFJvQTFrSmJ2K3ltSDNNS2dMbWNEUkNcL3JON0I5YmMwS2F1Q3BmRjI1aTRHT0tFODliRG5kWUpOUVZFVmxhIiwibWFjIjoiZjQ2ZmVhODU0OGQwOTUxZGU1ODAwMzE0MmI2ODUyN2MyY2QxZjhiY2QwNGI5N2M3NGQyNGExZTFkOWUyMDVjNSJ9'
             }
+        };
+
+    axios(config)
+        .then(function (response) {
+            // Show IP, datetime and user agent, response data
+            console.log(`IP: ${req.ip}, DateTime: ${new Date().toISOString()}, User-Agent: ${req.headers['user-agent']}, Response Data: ${JSON.stringify(response.data)}`);
+            res.status(200).send({
+                status: "successful",
+                payload: response.data
+            });
+        })
+        .catch(function (error) {
+            // Show IP, datetime and user agent, error
+            console.error(`IP: ${req.ip}, DateTime: ${new Date().toISOString()}, User-Agent: ${req.headers['user-agent']}, Error: ${error.message}`);
+            res.status(400).send({ status: "fail" });
         });
-        console.log(`IP: ${req.ip}, DateTime: ${new Date().toISOString()}, User-Agent: ${req.headers['user-agent']}, Response Data: ${JSON.stringify(response.data)}`);
-        res.status(200).send({
-            status: "successful",
-            payload: response.data
-        });
-    } catch (error) {
-        console.error(`IP: ${req.ip}, DateTime: ${new Date().toISOString()}, User-Agent: ${req.headers['user-agent']}, Error: ${error.message}`);
-        res.status(400).send({ status: "fail" });
-    }
 });
 
 
